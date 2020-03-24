ed8be7c66e4a48e39e9d596a39b81581810e102d
jhveem <joshua.hveem@gmail.com>
2020-03-24 10:45:38 -0600

    removing projects

--- CourseView.vue
+++ CourseView.vue
@@ -13,7 +13,7 @@
       </div>
       <div v-for="(todo, t) in todos" :key="t">
         <todo-item
-          v-if="((todo.pageType.includes(pageType) || pageType === '') && (todo.pageId === pageId || todo.pageId === ''))"
+          v-if="((todo.pageTypes.includes(pageType) || pageType === '') && (todo.pageId === pageId || todo.pageId === ''))"
           :todo="todo"
           :settings="userSettings"
           :open-tabs="openTabs"

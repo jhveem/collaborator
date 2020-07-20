13ed4382967aa3bfbce2a8c419cd13b68913ae48
Joshua Hveem <joshua.hveem@gmail.com>
2020-07-20 16:25:15 -0600

    create annotation library

--- libraries/annotation.js
+++ libraries/annotation.js
@@ -3,32 +3,58 @@ const ANNOTATOR = {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   },
 
-  getPathTo(elm) {
-    var allNodes = document.getElementsByTagName('*');
-    for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) {
-      if (elm.hasAttribute('id')) {
-        var uniqueIdCount = 0;
-        for (var n = 0; n < allNodes.length; n++) {
-          if (allNodes[n].hasAttribute('id') && allNodes[n].id == elm.id) uniqueIdCount++;
-          if (uniqueIdCount > 1) break;
-        };
-        if (uniqueIdCount == 1) {
-          segs.unshift('id("' + elm.getAttribute('id') + '")');
-          return segs.join('/');
-        } else {
-          segs.unshift(elm.localName.toLowerCase() + '[@id="' + elm.getAttribute('id') + '"]');
+  getPathTo(node) {
+    var comp, comps = [];
+    var parent = null;
+    var xpath = '';
+    var getPos = function(node) {
+        var position = 1, curNode;
+        if (node.nodeType == Node.ATTRIBUTE_NODE) {
+            return null;
         }
-      } else if (elm.hasAttribute('class')) {
-        segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]');
-      } else {
-        console.log('else');
-        for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) {
-          if (sib.localName == elm.localName) i++;
-        };
-        segs.unshift(elm.localName.toLowerCase() + '[' + i + ']');
-      };
-    };
-    return segs.length ? '/' + segs.join('/') : null;
+        for (curNode = node.previousSibling; curNode; curNode = curNode.previousSibling) {
+            if (curNode.nodeName == node.nodeName) {
+                ++position;
+            }
+        }
+        return position;
+     }
+
+    if (node instanceof Document) {
+        return '/';
+    }
+
+    for (; node && !(node instanceof Document); node = node.nodeType == Node.ATTRIBUTE_NODE ? node.ownerElement : node.parentNode) {
+        comp = comps[comps.length] = {};
+        switch (node.nodeType) {
+            case Node.TEXT_NODE:
+                comp.name = 'text()';
+                break;
+            case Node.ATTRIBUTE_NODE:
+                comp.name = '@' + node.nodeName;
+                break;
+            case Node.PROCESSING_INSTRUCTION_NODE:
+                comp.name = 'processing-instruction()';
+                break;
+            case Node.COMMENT_NODE:
+                comp.name = 'comment()';
+                break;
+            case Node.ELEMENT_NODE:
+                comp.name = node.nodeName;
+                break;
+        }
+        comp.position = getPos(node);
+    }
+
+    for (var i = comps.length - 1; i >= 0; i--) {
+        comp = comps[i];
+        xpath += '/' + comp.name;
+        if (comp.position != null) {
+            xpath += '[' + comp.position + ']';
+        }
+    }
+
+    return xpath;
   },
 
   getElementByXpath(path) {

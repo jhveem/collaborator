97f8141bb6afc08cd7528faeff4102e2d580d463
Joshua Hveem <joshua.hveem@gmail.com>
2020-07-20 14:18:28 -0600

    create annotation library

--- libraries/annotation.js
+++ libraries/annotation.js
@@ -3,55 +3,6 @@ const ANNOTATOR = {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   },
 
-  nextNode(node) {
-    if (node.hasChildNodes()) {
-      return node.firstChild;
-    } else {
-      while (node && !node.nextSibling) {
-        node = node.parentNode;
-      }
-      if (!node) {
-        return null;
-      }
-      return node.nextSibling;
-    }
-  },
-
-  getRangeSelectedNodes(range) {
-    var node = range.startContainer;
-    var endNode = range.endContainer;
-
-    // Special case for a range that is contained within a single node
-    if (node == endNode) {
-      return [node];
-    }
-
-    // Iterate nodes until we hit the end container
-    var rangeNodes = [];
-    while (node && node != endNode) {
-      rangeNodes.push(node = ANNOTATOR.nextNode(node));
-    }
-
-    // Add partially selected nodes at the start of the range
-    node = range.startContainer;
-    while (node && node != range.commonAncestorContainer) {
-      rangeNodes.unshift(node);
-      node = node.parentNode;
-    }
-
-    return rangeNodes;
-  },
-
-  getSelectedNodes() {
-    if (window.getSelection) {
-      var sel = window.getSelection();
-      if (!sel.isCollapsed) {
-        return ANNOTATOR.getRangeSelectedNodes(sel.getRangeAt(0));
-      }
-    }
-    return [];
-  },
-
   highlightRange(range, classId, color) {
     let className = 'canvas-collaborator-highlight-id-' + classId;
     var newNode = document.createElement("div");
@@ -60,6 +11,7 @@ const ANNOTATOR = {
       "background-color: " + color + "; display: inline;"
     );
     newNode.classList.add(className);
+    console.log(range);
     range.surroundContents(newNode);
   },
 

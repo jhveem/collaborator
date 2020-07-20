db010ead274cc8470892cd7332e05f71bb2955f6
Joshua Hveem <joshua.hveem@gmail.com>
2020-07-20 10:53:07 -0600

    add selection library from jquery

--- import.js
+++ import.js
@@ -1,15 +1,15 @@
-$.put = function(url, data){
+$.put = function (url, data) {
   return $.ajax({
-		url: url,
-		data: data,
+    url: url,
+    data: data,
     type: 'PUT'
   });
 }
 
-$.delete= function(url, data){
+$.delete = function (url, data) {
   return $.ajax({
-		url: url,
-		data: data,
+    url: url,
+    data: data,
     type: 'DELETE'
   });
 }
@@ -18,7 +18,7 @@ $.delete= function(url, data){
 async function _init() {
   //IMPORT VUE FILE AS STRING
   let vueString = '';
-  await $.get('https://jhveem.xyz/collaborator/CourseView.vue', null, function(html) {
+  await $.get('https://jhveem.xyz/collaborator/CourseView.vue', null, function (html) {
     vueString = html.replace("<template>", "").replace("</template>", "");
   }, 'text');
   //SET UP THE PAGE
@@ -28,23 +28,24 @@ async function _init() {
   //OR once it's all hosted on my site and on github, then updates will be instant as well
   canvasbody.after('<div id="canvas-collaborator-container"></div>');
   $('#left-side').append("<a id='canvas-collaborator-toggler' class='btn'>Collaborator</a>")
-  $("#canvas-collaborator-toggler").click(function() {
-  APP.toggleWindow();
+  $("#canvas-collaborator-toggler").click(function () {
+    APP.toggleWindow();
   });
   $("#canvas-collaborator-container").hide();
   $("#canvas-collaborator-container").append(vueString);
 
   //IMPORT EVERYTHING
   //Look into moving all of the libraries local that are hosted elsewhere
-  $.getScript("https://cdn.jsdelivr.net/npm/vue").done(function() {
-    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js").done(function() {
-      $.getScript("https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js").done(function() {
-        $.getScript("https://jhveem.xyz/collaborator/libraries/APIFunctions.js").done(function() {
-          $.getScript("https://jhveem.xyz/collaborator/libraries/SettingsFunctions.js").done(function() {
-            $.getScript("https://jhveem.xyz/collaborator/components/MenuItem.js").done(function() {
-              $.getScript("https://jhveem.xyz/collaborator/components/Modals.js").done(function() {
-                $.getScript("https://jhveem.xyz/collaborator/components/ChosenSelect.js").done(function() {
-                  $.getScript("https://jhveem.xyz/collaborator/vue.js").done(function() {
+  $.getScript("https://cdn.jsdelivr.net/npm/vue").done(function () {
+    $.getScript("https://jquery.selection.js").done(function () {
+      $.getScript("https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js").done(function () {
+        $.getScript("https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js").done(function () {
+          $.getScript("https://jhveem.xyz/collaborator/libraries/APIFunctions.js").done(function () {
+            $.getScript("https://jhveem.xyz/collaborator/libraries/SettingsFunctions.js").done(function () {
+              $.getScript("https://jhveem.xyz/collaborator/components/MenuItem.js").done(function () {
+                $.getScript("https://jhveem.xyz/collaborator/components/Modals.js").done(function () {
+                  $.getScript("https://jhveem.xyz/collaborator/components/ChosenSelect.js").done(function () {
+                    $.getScript("https://jhveem.xyz/collaborator/vue.js").done(function () {});
                   });
                 });
               });
